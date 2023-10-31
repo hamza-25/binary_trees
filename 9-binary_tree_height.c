49e2d226f08075e0c69a5c2104d3988a3ead9bef
hamza-25 <me677499@gmail.com>
2023-10-31 16:04:19 +0000

    calculte height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,13 +8,11 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t count = 0;
+	size_t right_h, left_h;
 
 	if (!tree)
 		return (0);
-	if (tree->left)
-		count++;
-	if (tree->right)
-		count++;
-	return (count);
+	left_h = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_h = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (right_h > left_h ? right_h : left_h);
 }
