16e4c625f6e872abdd9982f24a2bf1b94086c441
hamza-25 <me677499@gmail.com>
2023-10-31 14:21:18 +0000

    add function node leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - func that check if node leaf
+ * @node: node to be chechked
+ * Return: 1 if leaf otherwise 0
+*/
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+	if (node->left && node->right)
+		return (1);
+	return (0);
+}
