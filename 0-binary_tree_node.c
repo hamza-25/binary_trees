719f72c290236591d9b0dbb89e62c685c3ff456e
hamza-25 <me677499@gmail.com>
2023-10-31 11:25:48 +0000

    add funct that create node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - func that create node of tree
+ * @parent: parent of new node
+ * @value: value of the node
+ * Return: new node created
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node != NULL)
+	{
+		new_node->n = value;
+		new_node->left = NULL;
+		new_node->right = NULL;
+		if (parent != NULL)
+			new_node->parent = parent;
+	}
+	return (new_node);
+}
