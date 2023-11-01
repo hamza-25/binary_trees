1e29d5c170f832d4bf486ca55fccbd25514d96a9
hamza-25 <me677499@gmail.com>
2023-11-01 16:03:49 +0000

    add task 14 and 15

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,18 +1,30 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_heights - func that height
+ * @tree: start node
+ * Return: biggest height
+*/
+
+int binary_tree_heights(const binary_tree_t *tree)
+{
+	size_t right_h = 0, left_h = 0;
+
+	if (!tree)
+		return (0);
+	left_h = tree->left ? 1 + binary_tree_heights(tree->left) : 1;
+	right_h = tree->right ? 1 + binary_tree_heights(tree->right) : 1;
+	return (left_h > right_h ? left_h : right_h);
+}
+
+/**
  * binary_tree_balance - func that count at least one child
  * @tree: start node
  * Return: balanced number
 */
-
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t right_h, left_h;
-
 	if (!tree)
 		return (0);
-	left_h = binary_tree_balance(tree->left);
-	right_h = binary_tree_balance(tree->right);
-	return (left_h - right_h);
+	return (binary_tree_heights(tree->left) - binary_tree_heights(tree->right));
 }
---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - func that count at least one child
+ * @tree: node to be count from
+ * Return: 1 if full else 0
+*/
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int left_full, right_full;
+
+	if (!tree)
+		return (1);
+	if ((!tree->left && !tree->right) || (tree->left && tree->right))
+	{
+		left_full = binary_tree_is_full(tree->left);
+		right_full = binary_tree_is_full(tree->right);
+		return (left_full && right_full);
+	}
+	return (0);
+}
