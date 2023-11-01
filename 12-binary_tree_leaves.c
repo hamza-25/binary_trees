ada67d65a717fea4bb9b40d5244abfe9d0f7fa2c
hamza-25 <me677499@gmail.com>
2023-11-01 11:49:07 +0000

    count leaves in binary tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,16 +8,14 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaf = 0;
+	size_t leaf_left, leaf_right;
 
 	if (!tree)
 		return (0);
-	if (tree->left)
-		leaf++;
-	if (tree->right)
-		leaf++;
 	if (!tree->left && !tree->right)
-		leaf++;
-	return (leaf);
+		return (1);
+	leaf_left = binary_tree_leaves(tree->left);
+	leaf_right = binary_tree_leaves(tree->right);
+	return (leaf_left + leaf_right);
 
 }
