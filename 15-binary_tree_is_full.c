ca88e7bf1f40de11f8442ff628bf4ec383f5895e
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-11-01 17:06:22 +0100

    Update 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,7 +11,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	int left_full, right_full;
 
 	if (!tree)
-		return (1);
+		return (0);
 	if ((!tree->left && !tree->right) || (tree->left && tree->right))
 	{
 		left_full = binary_tree_is_full(tree->left);
