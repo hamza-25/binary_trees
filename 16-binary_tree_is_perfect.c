841159e271f8223808291e21a91dbd5d3c15a474
Ahmad Mahmoud Mohamad <55003947+Ahmadelhosary@users.noreply.github.com>
2023-11-02 23:55:13 +0200

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,1 +1,8 @@
+#include "binary_trees.h"
 
+int binary_tree_is_perfect(const binary_tree_t *tree);
+int is_perfect_recursive(const binary_tree_t *tree,
+		size_t leaf_depth, size_t level);
+const binary_tree_t *get_leaf(const binary_tree_t *tree);
+size_t depth(const binary_tree_t *tree);
+unsigned char is_leaf(const binary_tree_t *node);
